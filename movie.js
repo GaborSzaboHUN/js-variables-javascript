80c6be91a740e11bd5330ceccf869c07c56edd45
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-04-25 00:39:04 +0200

    updated with likes section

---
+++ movie.js
@@ -0,0 +1,240 @@
+// ---------- Movie ----------
+
+let title = "The Matrix"
+
+let length = 136
+
+let isKidFriendly = false
+
+let protagonist = {
+    "actorName": "Keanu Reeves",
+    "characterName": "Neo"
+}
+
+
+let antagonist = {
+    "actorName": "Hugo Weaving",
+    "characterName": "Agent Smith"
+}
+
+let myFavoriteMovie = {
+    "title": "The Matrix",
+    "releaseDate": 1999,
+    "directors": [ "Lana Wachowski", "Lilly Wachowski" ],
+    "productionCompany": "Warner Bros.",
+    "aspectRatio": "2.39 : 1",
+    "genres": [ "Sci-Fi", "Action" ],
+    "soundMix": [ "Dolby Digital", "SDDS", "Dolby Atoms" ],
+    "actors": [
+        { "actorName": "Keanu Reeves", "characterName": "Neo", "protagonist": true},
+        { "actorName": "Laurence Fishburne", "characterName": "Morpheus", "protagonist": true},
+        { "actorName": "Carrie-Anne Moss", "characterName": "Trinity", "protagonist": true},
+        { "actorName": "Hugo Weaving", "characterName": "Agent Smith", "protagonist": false},
+        { "actorName": "Gloria Foster", "characterName": "Oracle", "protagonist": true},
+        { "actorName": "Joe Pantoliano", "characterName": "Cypher", "protagonist": false},
+        { "actorName": "Marcus Chong", "characterName": "Tank", "protagonist": true},
+        { "actorName": "Julian Arahanga", "characterName": "Apoc", "protagonist": true},
+        { "actorName": "Matt Doran", "characterName": "Mouse", "protagonist": true},
+        { "actorName": "Belinda McClory", "characterName": "Switch", "protagonist": true},
+        { "actorName": "Anthony Ray Parker", "characterName": "Dozer", "protagonist": true},
+        { "actorName": "Paul Goddard", "characterName": "Agent Brown", "protagonist": false},
+        { "actorName": "Robert Taylor", "characterName": "Agent Jones", "protagonist": false},
+    ],
+    "awards": [
+        {"awardsType": "Oscar", "dateOfAward": 2000, "awardFor": [ "Best film editing", "Best sound", "Best effects", "Sound effects Editing", "Best effects, Visual Effects"
+        ]},
+        {"awardsType": "BAFTA Film Award", "dateOfAward": 2000, "awardFor": [ "Best Sound", "Best Achievement in Special Visual Effects"]},
+    ],
+
+}
+
+// ---------- Facebook character ----------
+let facebookFriends = {
+    "listOfFriends": [
+        { "name": "József Ecseki" },
+        { "name": "Katalin David" },
+        { "name": "Boglárka Lukáts" },
+        { "name": "Zoltán Márton" },
+        { "name": "Szabolcs Lakatos" },
+        { "name": "Shanel Kristóf" },
+        { "name": "Gyuszi Bódi" },
+        { "name": "Eszter Tiringer" },
+        { "name": "Dóri Kökény" },
+        { "name": "Vivi Agárdi" },
+        { "name": "Zoltán Samu" },
+        { "name": "Kokány Vivien Gréta" },
+        { "name": "Gecő Afroditte" },
+        { "name": "Andrea Fekecs" },
+        { "name": "Pozsár Noemi" },
+        { "name": "Zsuzsa Puskás" },
+        { "name": "Alexandra Hab" },
+        { "name": "Brigi Némethné Kiss-Vámosi" },
+        { "name": "Czekus Dorottya" },
+        { "name": "Hanusz Tamás" },
+        { "name": "Richárd G. Solymosi" },
+        { "name": "Dóri Szkladányi" },
+        { "name": "Vanessza Bianka Szabó" },
+        { "name": "Roland Mándoki" },
+        { "name": "Kiara Sebesi" },
+        { "name": "Bíró Levi" },
+        { "name": "Bernadett Wágner" },
+        { "name": "Petra Szkladányi" },
+        { "name": "Tomi Gubik" },
+        { "name": "Erika Nyulasi" },
+        { "name": "Tibor Bankó" },
+        { "name": "Pálné Wrzava" },
+        { "name": "Réka Kaszás" },
+        { "name": "Regina Fejes" },
+        { "name": "Reka Keresztes" },
+        { "name": "Méhn Melinda" },
+        { "name": "Hlavaj Dóri" },
+        { "name": "László Bodó" },
+        { "name": "Tóth Lacóó" },
+        { "name": "Christiaan J. Peters" },
+        { "name": "Bernadett Király" },
+        { "name": "Horváth Márton" },
+        { "name": "Szilagyi Tamas" },
+        { "name": "Bálint Juhász" },
+        { "name": "Ádám Nagy" },
+        { "name": "Nikolett Hartai" },
+        { "name": "Dorka Sallai" },
+        { "name": "Tamara Juhász" },
+        { "name": "Sarkadi Barnabás" },
+        { "name": "Éva Urbán" },
+        { "name": "Barnabás Rácz" },
+        { "name": "Levi Lipcsei" },
+        { "name": "Szampiás Zoltán" },
+        { "name": "Pál Istók" },
+        { "name": "Eloise Debono" },
+        { "name": "Zsolt Kámán" },
+        { "name": "István Milihovszki" },
+        { "name": "Tamás Furkó" },
+        { "name": "Kiss Gergely" },
+        { "name": "Józsefné Kati Spekker" },
+        { "name": "Balázs Nagy" },
+        { "name": "Kámán-Tompek Nikolett" },
+        { "name": "Abigél Bajó" },
+        { "name": "Viktória Müller" },
+        { "name": "Barna Krajcsovich" },
+        { "name": "Alexandra Belovics" },
+        { "name": "Soma Sáhó" },
+        { "name": "Vivien Vincze" },
+        { "name": "Csordás Flóra" },
+        { "name": "József Spekker" },
+        { "name": "Balázs Csete" },
+        { "name": "Laura Dancsák" },
+        { "name": "Péter Becsei" },
+        { "name": "János Nóvé" },
+        { "name": "András Páli" },
+        { "name": "Márton Keczkó" },
+        { "name": "Enikő Páli-Vejmola" },
+        { "name": "Nóra Faggyas" },
+        { "name": "Kator Katalin" },
+        { "name": "Attila Kőrösi" },
+        { "name": "Éva Bosányi" },
+        { "name": "Éva Köbölné Hilarion" },
+        { "name": "Veresné Vezér Olga" },
+        { "name": "Mária Huszár" },
+        { "name": "Istvan Simon" },
+        { "name": "Luca Nagy" },
+        { "name": "Akos Porcs-Makkay" },
+        { "name": "Miskei Roxána" },
+        { "name": "Szilvia Salzmann" },
+        { "name": "Gergely Fogarasi" },
+        { "name": "Márti Dömsödi" },
+        { "name": "Buda Roland" },
+        { "name": "Király Dávid" },
+        { "name": "Tundee Thomas" },
+        { "name": "Varsányi Bence" },
+        { "name": "Orvos Brendon" },
+        { "name": "Tibor Gősi" },
+        { "name": "Faludy Zsanett" },
+        { "name": "Erőssné Makovecz Ildikó" },
+        { "name": "Krisztián Kupai" },
+        { "name": "Anikó Péter" },
+        { "name": "Gábor Németh" },
+        { "name": "Borka Skrapits" },
+        { "name": "Viktória Zelényi" },
+        { "name": "Jana Voros" },
+        { "name": "Eleni Georgopulosz" },
+        { "name": "Bogi Tóth-Szabó" },
+        { "name": "Katica Hunyadi" },
+        { "name": "Istvánné Deme" },
+        { "name": "Ruben Bajó" },
+        { "name": "Márk Polákovits" },
+        { "name": "Döme Rozsnyói" },
+        { "name": "Dina Naszvadi" },
+        { "name": "Anna Madar" },
+        { "name": "Balás Fanni" },
+        { "name": "Gábor Karacs" },
+        { "name": "Gábor Kovács" },
+        { "name": "Gabriella Szabó" },
+        { "name": "Szentes Anna" },
+        { "name": "Péter Tóth" },
+        { "name": "Dávid Danyi" },
+        { "name": "Kristóf Deli" },
+        { "name": "Bálint Horváth" },
+        { "name": "Anka Fehér" },
+        { "name": "Pencz Attila Tamás" },
+        { "name": "Michael Ru" },
+        { "name": "Attila Juhász" },
+        { "name": "Renátó Hegedűs" },
+        { "name": "Ráduly István" },
+        { "name": "Enikő Ücveges" },
+        { "name": "Orsolya Berecz" }
+      ],
+}
+
+
+let myProfile = {
+    "username": "Gábor Szabó",
+    "friendsNo": 659,
+    "friends": facebookFriends,
+    "likes": {
+        "music": ["Linkin Park", "Robbie Williams", "Slash", "Smash into peaces", "Guns N' Roases", "Bon Jovi", "Fall out boys"],
+        "movies": ["Matrix", "The Dark Knight", "Pulp Fiction", "Inception", "Terminator 2: Judgment Day", "The Prestige", "Back to the Future"],
+        "sports": ["Functional training", "BJJ", "Karate"],
+        "books": ["Game of thrones series", "Harry Potter series", "Dune series", "Miss PEregien Home for peculiar children", "Extreme ownership", "Digital fortress"]
+    },
+    "comments": 155,
+    "personalInformation": [
+        {"visible": false, "married": true, "currentJob": "Operations Manager at Superfly Air Sports Budapest", "sex": "male",
+    },
+    ],
+    "imageAlbums": [
+        {"profileImages": 2},
+        {"mobileImages": 7},
+        {"timelineImages": 1},
+        {"szigetImages": 4},
+        {"kettlebellTrainingCamp2015Images": 148},
+        {"london2014Images": 24},
+        {"dublin2012Images": 34},
+    ],
+    "imagesWithHair": 0,
+    "followers": 15,
+    "lifeEvents": 8,
+}
+
+
+// ---------- One more ----------
+
+let bookShelf = {
+    "color": "black",
+    "size": [
+        {"Height": 2020},
+        {"Width": 1200},
+        {"Depth": 280},
+    ],
+    "numberOfShelf": [
+        {"numberOfAllShelf": 18},
+        {"rowOfShelf": 6},
+        {"columnOfShelf": 3}
+    ],
+    "sectionsOfBooks": 13,
+    "nonBookRelatedItems": [
+        {"numberOfNonBookRelatedItems": 13},
+        "statue", "mug", "money box", "digital artboard", "binoculars", "folders", "socket strip"
+    ]
+}
+
+console.log(myFavoriteMovie, myProfile, bookShelf)
